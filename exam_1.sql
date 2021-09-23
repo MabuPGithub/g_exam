34bd2ce12cc0313c7f2e7d7b6b924c4dd9147d84
Myles Mempin <badger2pt0@gmail.com>
2021-09-23 20:00:45 +0800

    question 3 answer added

--- exam_1.sql
+++ exam_1.sql
@@ -12,3 +12,7 @@ where salary_id = 3;
 SELECT * FROM Employees
 WHERE date_hired BETWEEN '2017-01-01' AND '2018-12-31';
 
+-- Get all employees that belongs to IT dept and hired 2018 onwards
+SELECT * FROM Employees
+WHERE department_id = 3
+AND date_hired > '2018-01-01';
